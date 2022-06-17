d4675266b8a0d8b0d510f993ac6a993e2ed493ca
luxuefeng <luxuefeng@xiaozhu.com>
2022-06-17 12:34:03 +0800

    map reduce

---
+++ demo26/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 函数体都是在遍历第一个参数的数组，然后，调用第二个参数的函数，然后把其值组合成另一个数组返回。
+func MapStrToStr(arr []string, fn func(s string) string) []string {
+	var newArray = []string{}
+	for _, it := range arr {
+		newArray = append(newArray, fn(it))
+	}
+	return newArray
+}
+
+func MapStrToInt(arr []string, fn func(s string) int) []int {
+	var newArray = []int{}
+	for _, it := range arr {
+		newArray = append(newArray, fn(it))
+	}
+	return newArray
+}
+
+func Reduce(arr []string, fn func(s string) int) int {
+	sum := 0
+	for _, it := range arr {
+		sum += fn(it)
+	}
+	return sum
+}
+
+func Filter(arr []int, fn func(n int) bool) []int {
+	var newArray = []int{}
+	for _, it := range arr {
+		if fn(it) {
+			newArray = append(newArray, it)
+		}
+	}
+	return newArray
+}
+
+func main() {
+	var list = []string{"Hao", "Chen", "MegaEase"}
+
+	x := MapStrToStr(list, func(s string) string {
+		return strings.ToUpper(s)
+	})
+	fmt.Printf("%v\n", x)
+	//["HAO", "CHEN", "MEGAEASE"]
+
+	y := MapStrToInt(list, func(s string) int {
+		return len(s)
+	})
+	fmt.Printf("%v\n", y)
+	//[3, 4, 8]
+	//////////////////////
+
+	var list2 = []string{"Hao", "Chen", "MegaEase"}
+
+	x2 := Reduce(list2, func(s string) int {
+		return len(s)
+	})
+	fmt.Printf("%v\n", x2)
+	// 15
+
+	var intset = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	out := Filter(intset, func(n int) bool {
+		return n%2 == 1
+	})
+	fmt.Printf("%v\n", out)
+
+	out = Filter(intset, func(n int) bool {
+		return n > 5
+	})
+	fmt.Printf("%v\n", out)
+
+}
