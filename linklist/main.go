5cff5c936d5ca0c2a9374e0c5928642ce9c8dc2a
luxuefeng <luxuefeng@xiaozhu.com>
2022-06-27 17:12:57 +0800

    链表

--- linklist/main.go
+++ linklist/main.go
@@ -128,3 +128,107 @@ func rever2(head *node) *node {
 	}
 	return pre
 }
+
+/*
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock
+
+*/
+func maxProfit(prices []int) int {
+	res := 0
+	minPrice := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+		if prices[i]-minPrice > res {
+			res = prices[i] - minPrice
+		}
+	}
+	return res
+}
+
+///////////// 给定一个排序链表，删除全部重复的元素，使得每一个元素只出现一次。////////////////
+func deleteDuplicates(head *node) *node {
+	curr := head
+	for curr != nil && curr.next != nil {
+		if curr.data == curr.next.data {
+			curr.next = curr.next.next
+		} else {
+			curr = curr.next
+		}
+	}
+	return head
+}
+
+// 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates2(head *ListNode) *ListNode {
+	curr := head
+	record := map[int]int{}
+
+	for curr != nil {
+		record[curr.Val] = record[curr.Val] + 1 //记录每个元素的数量
+		curr = curr.Next
+	}
+
+	curr = head
+	for curr != nil && curr.Next != nil {
+		if record[curr.Next.Val] > 1 {
+			//需要删除
+			curr.Next = curr.Next.Next
+		} else {
+			curr = curr.Next
+		}
+	}
+	//判断head是否
+	if head != nil && record[head.Val] > 1 {
+		head = head.Next
+	}
+
+	return head
+}
+
+func deleteDuplicates3(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	dummy := &ListNode{0, head}
+
+	cur := dummy
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			x := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == x {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return dummy.Next
+}
