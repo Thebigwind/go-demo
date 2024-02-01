74276a22df57db0bc46d36b90e8f520f409ec4f8
luxf <luxf@gzdl.cc>
2024-02-01 14:42:08 +0800

    sm3摘要

--- common/sm3.go
+++ common/sm3.go
@@ -1,1 +1,14 @@
 package common
+
+import (
+	"encoding/hex"
+	"github.com/tjfoc/gmsm/sm3"
+)
+
+//sm3摘要
+func EncryptSM3(msg string) string {
+	hash := sm3.New()
+	hash.Write([]byte(msg))
+	bytes := hash.Sum(nil)
+	return hex.EncodeToString(bytes)
+}
